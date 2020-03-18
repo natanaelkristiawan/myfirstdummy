3b6f3a486f214582ba797d7fd6bebb2fd3e7062e
natanael kristiawan <natanaelkristiawan@maxsol.id>
2020-03-18 13:28:50 +0800

    Connection Activity

--- app/src/main/java/com/example/myfirstdummy/MainActivity.kt
+++ app/src/main/java/com/example/myfirstdummy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstdummy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             //code
             Log.i("MainActivity",  "Button was Clicked")
             Toast.makeText(this, "Button was Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
