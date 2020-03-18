55c2e87d4f48fe95f8374807f61c03b26a0ab9c3
natanael kristiawan <natanaelkristiawan@maxsol.id>
2020-03-18 15:32:09 +0800

    explisit intent

--- app/src/main/java/com/example/myfirstdummy/MainActivity.kt
+++ app/src/main/java/com/example/myfirstdummy/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnSendMsgToNextActivity.setOnClickListener {
-            val message: String = etUserMessage.text.toString()
+            val message: String = getUserMessage.text.toString()
             //Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, SecondActivity::class.java)
@@ -28,6 +28,19 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("user_message", message)
 
             startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener{
+
+            val message: String = getUserMessage.text.toString()
+
+            val intent = Intent()
+
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share To"))
         }
     }
 }
